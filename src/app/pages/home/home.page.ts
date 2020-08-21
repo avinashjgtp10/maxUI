5b3e9dbaa94ee70442fc6875f1892eb4c9db1225
Scdesai <sudatta.desai@harman.com>
2020-08-21 23:25:02 +0530

    MyPlansDesignComplete

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild } from '@angular/core';
 import { ActionSheetController } from '@ionic/angular';
 import { Storage } from "@ionic/storage";
 import { ModalController } from '@ionic/angular';
 import { CalorieTrackerPage } from '../calorie-tracker/calorie-tracker.page';
-
+import { IonSlides } from '@ionic/angular';
 import { Router,ActivatedRoute } from "@angular/router"
 import { WaterTrackerPage } from '../water-tracker/water-tracker.page';
 import { HandwashTrackerPage } from '../handwash-tracker/handwash-tracker.page';
@@ -17,14 +17,61 @@ export class HomePage implements OnInit {
   public items: any = [];
   isExpanded: boolean = false;
   trackersData: any = [];
+  liveClassesData: Array<Object> = [];
+  specialPlanesData: Array<Object> = [];
+  @ViewChild('liveClassesSlider', { static: false }) liveClassesSlider: IonSlides;
+  @ViewChild('offersSlider', { static: false }) offersSlider: IonSlides;
+  @ViewChild('specialPlansSlider', { static: false }) specialPlansSlider: IonSlides;
   constructor(public actionSheetController: ActionSheetController,private route:Router,
     private storage:Storage, public modalController: ModalController) {
    }
+   liveClassesOpts = {
+    initialSlide: 0,
+    slidesPerView: 2.5,
+    speed: 400
+    };
+    offersOpts = {
+      initialSlide: 0,
+      slidesPerView: 2.5,
+      speed: 400
+    };
+    specialPlansOpts = {
+      initialSlide: 0,
+      slidesPerView: 2.5,
+      speed: 400
+   };
   ngOnInit() {
+    this.liveClassesData = [{
+      name: 'Live1',
+      imgUrl: ''
+    },{
+      name: 'Live2',
+      imgUrl: ''
+    },{
+      name: 'Live3',
+      imgUrl: ''
+    },
+    {
+      name: 'Live4',
+      imgUrl: ''
+    }]
+    this.specialPlanesData = [{
+      title: 'Muscle Training',
+      imgUrl: '',
+      desc:'Lorem Ipsum is simply dummy text'
+    },{
+      title: 'Yoga',
+      imgUrl: '',
+      desc:'Lorem Ipsum is simply dummy text'
+    },{
+      title: 'Weight Loss',
+      imgUrl: '',
+      desc:'Lorem Ipsum is simply dummy text'
+    }]
   }
  
   openTrackerDetail(event){
-    console.log(event);
+   // console.log(event);
     this[event.actionName]();
   }
   async waterTracker() {
