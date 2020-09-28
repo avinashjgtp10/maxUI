fa7978a055f68c53da3d8512b3b77725245e2a9f
Avinash Jagtap <avinashjgtp10@gmail.com>
2020-09-28 16:05:49 +0530

    Resolved ngfor issue

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,23 +1,21 @@
-import { NgModule } from '@angular/core';
+import { NgModule } from "@angular/core";
 
-import { FormsModule } from '@angular/forms';
+import { FormsModule } from "@angular/forms";
 
-import { IonicModule } from '@ionic/angular';
+import { IonicModule } from "@ionic/angular";
 
-import { HomePageRoutingModule } from './home-routing.module';
-import { CommonModule } from '@angular/common';
+import { HomePageRoutingModule } from "./home-routing.module";
+import { CommonModule } from "@angular/common";
 
-import { HomePage } from './home.page';
-import { ComponentsModule } from '../../components/components.module';
-import { PipesModule } from '../../pipes/pipes.module';
-import { ToastProvider } from 'src/app/services/toast/toast';
-import { WeightTrackerPageModule } from '../weight-tracker/weight-tracker.module';
-import { HandwashTrackerPageModule } from '../handwash-tracker/handwash-tracker.module';
-import { WaterTrackerPageModule } from '../water-tracker/water-tracker.module';
-
-
-
-
+import { HomePage } from "./home.page";
+import { ComponentsModule } from "../../components/components.module";
+import { PipesModule } from "../../pipes/pipes.module";
+import { ToastProvider } from "src/app/services/toast/toast";
+import { WeightTrackerPageModule } from "../weight-tracker/weight-tracker.module";
+import { HandwashTrackerPageModule } from "../handwash-tracker/handwash-tracker.module";
+import { WaterTrackerPageModule } from "../water-tracker/water-tracker.module";
+import { AddCaloriePage } from "../add-calorie/add-calorie.page";
+import { CalorieTrackerPage } from '../calorie-tracker/calorie-tracker.page';
 
 @NgModule({
   imports: [
@@ -29,9 +27,9 @@ import { WaterTrackerPageModule } from '../water-tracker/water-tracker.module';
     HomePageRoutingModule,
     WeightTrackerPageModule,
     HandwashTrackerPageModule,
-    WaterTrackerPageModule
+    WaterTrackerPageModule,
   ],
-  declarations: [HomePage],
-  providers: [ToastProvider]
+  declarations: [HomePage, AddCaloriePage, CalorieTrackerPage],
+  providers: [ToastProvider],
 })
 export class HomePageModule {}
