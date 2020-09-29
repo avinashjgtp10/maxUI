aec4e705d1a37f5418c45dabeb2404769f6666a5
vinod chavan <vinodchavan102@gmail.com>
2020-09-29 14:07:02 +0530

    video player auto play changes

--- src/app/pages/work-out-video/work-out-video.page.ts
+++ src/app/pages/work-out-video/work-out-video.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
+import * as _ from 'lodash';
 @Component({
   selector: 'app-work-out-video',
   templateUrl: './work-out-video.page.html',
@@ -8,23 +9,23 @@ export class WorkOutVideoPage implements OnInit {
   public items: any = [];
   @Input() playlistData: any;
   @ViewChild("videoPlayer", { static: false }) videoplayer: ElementRef;
-  isPlaying:boolean = false;
-  playingVideoSrc:string;
-  currentPlayingItem:any;
-  completePlaylist:any;
-  currentPlayingIndex:any = 0;
+  isPlaying: boolean = false;
+  playingVideoSrc: string;
+  currentPlayingItem: any;
+  completePlaylist: any;
+  currentPlayingIndex: any = 0;
   constructor() { }
   ngOnInit() {
     //this.playingVideoSrc = 'https://maxfit.s3.amazonaws.com/videoplayback.mp4';
     this.completePlaylist = [...this.playlistData];
     this.currentPlayingItem = this.completePlaylist[this.currentPlayingIndex];
     this.playingVideoSrc = this.currentPlayingItem.tmvideourl;
+    this.completePlaylist = this.completePlaylist.map((ele) => {
+      return _.extend(ele, { isChecked: false })
+    })
   }
-  videoEnded(){
-    console.log('video ended');
-    let myVideo: any = document.getElementById("my_video_1");
-    myVideo.pause();
-    this.isPlaying = false;
+  videoEnded() {
+    this.completePlaylist[this.currentPlayingIndex].isChecked = true;
     this.next();
   }
   playVideo(event: any) {
@@ -32,42 +33,35 @@ export class WorkOutVideoPage implements OnInit {
     if (myVideo.paused) {
       myVideo.play();
       this.isPlaying = true;
-    } 
+    }
     else {
       myVideo.pause();
       this.isPlaying = false;
     }
   }
-  next(){
+  next() {
     let myVideo: any = document.getElementById("my_video_1");
-    console.log('this.completePlaylist',this.completePlaylist);
-     if(this.isPlaying){
-       this.currentPlayingIndex++;
-       console.log(this.currentPlayingIndex);
-       if(this.completePlaylist[this.currentPlayingIndex]){
-        myVideo.pause();
-        // myVideo.stop();
-        this.isPlaying = false;
-        this.currentPlayingItem = this.completePlaylist[this.currentPlayingIndex];
-        console.log('this.currentPlayingItem',this.currentPlayingItem);
-         this.playingVideoSrc = this.currentPlayingItem.tmvideourl;
-         console.log('myVideo',myVideo.currentSrc);
-        //  myVideo.play();
-        //  this.isPlaying = true;
-       }else {
-        console.log('Next is not allowed');
-       }
-     } else {
+    if (this.isPlaying) {
       this.currentPlayingIndex++;
-      if(this.completePlaylist[this.currentPlayingIndex]){
-        this.currentPlayingItem = this.playlistData[this.currentPlayingIndex];
-         this.playingVideoSrc = this.currentPlayingItem.tmvideourl;
-       }else {
+      if (this.completePlaylist[this.currentPlayingIndex]) {
+        this.currentPlayingItem = this.completePlaylist[this.currentPlayingIndex];
+        this.playingVideoSrc = this.currentPlayingItem.tmvideourl;
+        myVideo.src = this.currentPlayingItem.tmvideourl;
+        myVideo.play();
+      } else {
         console.log('Next is not allowed');
-       }
-     }
+      }
+    } else {
+      this.currentPlayingIndex++;
+      if (this.completePlaylist[this.currentPlayingIndex]) {
+        this.currentPlayingItem = this.playlistData[this.currentPlayingIndex];
+        this.playingVideoSrc = this.currentPlayingItem.tmvideourl;
+      } else {
+        console.log('Next is not allowed');
+      }
+    }
   }
-  pre(){
+  pre() {
 
   }
 }
